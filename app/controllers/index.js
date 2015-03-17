696360f30e8dee30a69aaf679091183ccdf96d1e
Nitin Pradeep Kumar <nkumar@appcelerator.com>
2015-03-17 11:50:44 +0800

    android actionbar support

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,1 +1,27 @@
-$.index.open();
+function doOpen() {
+   if (OS_ANDROID) {
+      var activity = $.getView().activity;
+      var menuItem = null;
+      
+      activity.onCreateOptionsMenu = function(e) {
+        if ($.tabGroup.activeTab.title === "Feed") {
+	        menuItem = e.menu.add({
+	          //itemId : "PHOTO",
+	           title : "Take Photo",
+	           showAsAction : Ti.Android.SHOW_AS_ACTION_ALWAYS,
+	           icon : Ti.Android.R.drawable.ic_menu_camera
+			});
+			menuItem.addEventListener("click", function(e) {
+				$.feedController.cameraButtonClicked();
+			}); 
+		}
+	  };
+      //activity.invalidateOptionsMenu();
+      // this forces the menu to update when the tab changes
+      $.tabGroup.addEventListener('blur', function(_event) {
+      	$.getView().activity.invalidateOptionsMenu();
+      });
+	} 
+}
+
+$.tabGroup.open();
