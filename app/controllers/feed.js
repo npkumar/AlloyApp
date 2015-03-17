7165e85e628b19e9ca2eb34f1064bae084524177
Nitin Pradeep Kumar <nkumar@appcelerator.com>
2015-03-17 15:26:55 +0800

    workaround for Ti.Media.Camera call issue for android

--- app/controllers/feed.js
+++ app/controllers/feed.js
@@ -6,28 +6,10 @@ OS_IOS && $.cameraButton.addEventListener("click",function(_event){
 $.cameraButtonClicked = function(_event) {
 	
    alert("user clicked camera button");
-   
-   var photoSource = Titanium.Media.getIsCameraSupported() ?
-       Titanium.Media.showCamera : Titanium.Media.openPhotoGallery;
        
-	photoSource ({
+	var photoSourceObj = {
       success : function(event) {
-		// processImage(event.media, function(photoResp) { 
-				// // create the row
-				// var row = Alloy.createController("feedRow", photoResp);
-				// // add the controller view, which is a row to the table 
-				// if ($.feedTable.getData().length === 0) {
-         			// $.feedTable.setData([]);
-					// $.feedTable.appendRow(row.getView(), true); 
-				// } else {
-           			// $.feedTable.insertRowBefore(0,row.getView(), true);
-        		// }
-		// });
-		
-			   var photoResp = {
-				    image : event.media,
-				    title : "Sample Photo " + new Date()
-				};
+		processImage(event.media, function(photoResp) { 
 				// create the row
 				var row = Alloy.createController("feedRow", photoResp);
 				// add the controller view, which is a row to the table 
@@ -37,6 +19,8 @@ $.cameraButtonClicked = function(_event) {
 				} else {
            			$.feedTable.insertRowBefore(0,row.getView(), true);
         		}
+		});
+		
 	  },
       cancel : function() {
        // called when user cancels taking a picture
@@ -53,7 +37,12 @@ $.cameraButtonClicked = function(_event) {
        allowEditing : true,
        // only allow for photos, no video
        mediaTypes : [Ti.Media.MEDIA_TYPE_PHOTO]
-	}); 
+	}; 
+
+   //feons - cannot assign Titanium.Media.showCamera to a variable in android :(
+   Titanium.Media.getIsCameraSupported() ?
+       Titanium.Media.showCamera(photoSourceObj) : Titanium.Media.openPhotoGallery(photoSourceObj);
+       
 };
 
 function processImage(_mediaObject, _callback) {
